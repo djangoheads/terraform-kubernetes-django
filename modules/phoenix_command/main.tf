0ad56772f817cc7f1d6bfba1ce2fec0e50a957a3
Stebukov <you.step.me@gmail.com>
2025-02-19 16:34:53 +0000

    fix: added phoenix_command

---
+++ modules/phoenix_command/main.tf
@@ -0,0 +1,53 @@
+resource "kubernetes_job" "default" {
+  metadata {
+    namespace = var.namespace
+    name      = var.name
+    labels = var.labels
+  }
+  spec {
+    ttl_seconds_after_finished = 100
+    backoff_limit              = 5
+    template {
+      metadata {
+        labels = merge({
+          service         = var.name
+          config_revision = var.config_revision
+          secret_revision = var.secret_revision
+        }, var.labels)
+      }
+      spec {
+        container {
+          name              = "main"
+          image             = var.image
+          command           = var.command
+          args              = var.args
+          image_pull_policy = var.image_pull_policy
+          working_dir = var.working_dir
+          resources {
+            limits   = length(var.limits) > 0 ? var.limits : null
+            requests = length(var.requests) > 0 ? var.requests : null
+          }
+          dynamic "env" {
+            for_each = var.env_vars
+            content {
+              name  = env.key
+              value = env.value
+            }
+          }
+
+          # Mounts
+          env_from {
+              config_map_ref {
+                name = "${var.name}-settings"
+              }
+              secret_ref {
+                name = "${var.name}-settings"
+              }
+            }
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+  wait_for_completion = var.wait
+}
---
+++ modules/phoenix_command/variables.tf
@@ -0,0 +1,126 @@
+# Common variables
+
+variable "namespace" {
+    type = string
+}
+
+variable "name" {
+    type = string
+}
+
+# Server specific variables
+variable "image" {
+  type        = string
+  description = "image"
+  default     = ""
+}
+
+variable "command" {
+  type        = list(string)
+  default     = []
+  description = "Override command"
+}
+
+variable "args" {
+  type        = list(string)
+  default     = []
+  description = "Override command"
+}
+
+variable "wait" {
+  type        = bool
+  default     = true
+  description = "Perform waiting resource to be available and run"
+}
+variable "env_vars" {
+  description = "Environment variables for the storage module"
+  type        = map(string)
+  default     = {}
+}
+
+variable "configmap_name" {
+  type        = string
+  description = "Configmap name"
+  default     = null
+}
+
+variable "secret_name" {
+  type        = string
+  description = "Secret name"
+  default     = null
+}
+
+variable "settings_mount_path" {
+  type    = string
+  default = "/home/app/config/override.yaml"
+}
+
+variable "secrets_mount_path" {
+  type    = string
+  default = "/home/app/config/.secrets.yaml"
+}
+
+variable "image_pull_policy" {
+  type        = string
+  default     = "Always"
+  description = "Image pull policy"
+}
+
+variable "configmap_key" {
+  type = string
+  default = "override.yaml"
+  description = "The key of the configmap to be used as the configuration for the application"
+}
+
+variable "configmap_path" {
+  type = string
+  default = "override.yaml"
+  description = "The path where the configmap will be mounted"
+}
+
+variable "secret_key" {
+  type = string
+  default = ".secrets.yaml"
+  description = "The key of the secret to be used as the configuration for the application"
+}
+
+variable "secret_path" {
+  type = string
+  default = ".secrets.yaml"
+  description = "The path where the secret will be mounted"
+}
+
+variable "working_dir" {
+  type = string
+  default = "/home/app/libs"
+  description = "The working directory for the container"
+}
+
+variable "config_revision" {
+  type = string
+  default = ""
+  description = "The revision of the configmap to be used"
+}
+
+variable "secret_revision" {
+  type = string
+  default = ""
+  description = "The revision of the secret to be used"
+}
+
+variable "limits" {
+  description = "Resource limits (CPU and memory)"
+  type        = map(string)
+  default     = {}
+}
+
+variable "requests" {
+  description = "Resource requests (CPU and memory)"
+  type        = map(string)
+  default     = {}
+}
+variable "labels" {
+  description = "Labels to be added to the deployment"
+  type        = map(string)
+  default     = {}
+}
